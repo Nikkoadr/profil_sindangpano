8d8ed1cb56242561a3087be2c32f6f3b4fb4faaf
Nikko Adrian <nikkoadrian02@gmail.com>
2025-08-28 15:16:10 +0700

    Update hero sections for UMKM and Wisata pages
    Enhanced the hero headers in umkm.blade.php and wisata.blade.php with new layouts, breadcrumb navigation, and updated titles for improved user experience and consistency.

--- resources/views/umkm.blade.php
+++ resources/views/umkm.blade.php
@@ -3,7 +3,19 @@
 
 <div class="container-fluid pb-5 bg-primary hero-header">
     <div class="container py-5">
-        <h1 class="display-3 text-white">UMKM Desa</h1>
+        <div class="row g-3 align-items-center">
+            <div class="col-lg-6 text-center text-lg-start">
+                <h1 class="display-1 mb-0 animated slideInLeft">UMKM Lokal</h1>
+            </div>
+            <div class="col-lg-6 animated slideInRight">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb justify-content-center justify-content-lg-end mb-0">
+                        <li class="breadcrumb-item"><a class="text-primary" href="#">Beranda</a></li>
+                        <li class="breadcrumb-item text-secondary active" aria-current="page">UMKM Lokal</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
     </div>
 </div>
 
