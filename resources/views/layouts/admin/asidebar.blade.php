a9d2bcd0f335b865ef4d54be07c53687b2367440
Nikko Adrian <nikkoadrian02@gmail.com>
2025-08-29 08:54:40 +0700

    Update asidebar.blade.php

--- resources/views/layouts/admin/asidebar.blade.php
+++ resources/views/layouts/admin/asidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 <!-- Brand Logo -->
 <a href="index3.html" class="brand-link">
-    <img src="{{ asset('assets/img/Logo.png') }}" alt="Sindangpano" class="brand-image elevation-3" style="opacity: .8">
+    <img src="{{ asset('assets/img/logo.png') }}" alt="Sindangpano" class="brand-image elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light">Sindangpano</span>
 </a>
 
